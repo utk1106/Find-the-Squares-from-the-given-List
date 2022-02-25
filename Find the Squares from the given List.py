24e3354ce22f7d763f5c54ae30ae517c55835707
Utkarsh <thaokarutkarsh@gmail.com>
2022-02-25 22:16:54 +0530

    Add files via upload

---
+++ Find the Squares from the given List.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+def sq_no(n):
+    return n*n
+
+
+# In[12]:
+
+
+no=[4, 5, 2, 9]
+
+
+# In[13]:
+
+
+a = map(sq_no,no)
+
+
+# In[14]:
+
+
+print(a)
+
+
+# In[15]:
+
+
+print(list(a))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
